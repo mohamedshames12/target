ea569e88825ce10f057ad4928b7733faa75e85fb
mohamed shames <mohamedhames900@gmail.com>
2023-04-15 18:08:53 +0200

    update

---
+++ static/assets/js/script.js
@@ -0,0 +1,17 @@
+const btnMenu = document.getElementById("menu");
+const navbar = document.querySelector(".navbar");
+const close = document.querySelector("#close");
+btnMenu.addEventListener("click", () => {
+    navbar.classList.add("active");
+    setTimeout(() => { 
+        close.style.display = "block";
+     }, 300)
+})
+
+close.addEventListener("click", () => {
+    navbar.classList.remove("active");
+    setTimeout(() => { 
+        close.style.display = "none";
+     }, 250)
+})
+
