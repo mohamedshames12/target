9cc6774359b8d0d307d230d2931120da4c94e138
mohamed shames <mohamedhames900@gmail.com>
2023-04-16 03:33:19 +0200

    update

--- core/urls.py
+++ core/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('settings', views.settings, name='settings'),
     path('register', views.register, name='register'),
     path('login', views.login, name='login'),
+    path('logout', views.logout, name='logout'),
 ]
--- core/views.py
+++ core/views.py
@@ -2,14 +2,23 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import User, auth
 from django.contrib import messages
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 from .models import Profile
+
+
 # Create your views here.
+
+# this is functionality to allow you to go index page
 
 def index(request):
     return render(request, 'index.html')
 
+# this is functionality to allow you to go settings page
+@login_required(login_url='settings')
+def settings(request):
+    return render(request, 'settings.html')
 
-
+# I created a database connection to register
 def register(request):
     
     if request.method == 'POST':
@@ -30,23 +39,41 @@ def register(request):
                 user.save()
                 
                 #log user in and redirect to settings page
+                user_login = auth.authenticate(username=username, password=password)
+                auth.login(request,user_login)
                 
                 #create a profile object for the new user
                 user_model = User.objects.get(username=username)
                 new_profile = Profile.objects.create(user=user_model, id_user=user_model.id)
                 new_profile.save()
-                return redirect('register')
+                return redirect('settings')
         else:
-            messages.info(request ,'Password Not Correct!')
+            messages.info(request ,'Pass    word Not Correct!')
             return redirect('register')
         
     else:   
         return render(request, 'register.html')
 
-
-
-
-
-
+# I created a database connection to login
 def login(request):
-    return render(request, 'login.html')
+    
+    if request.method == 'POST':
+        username = request.POST['name']
+        password = request.POST['password']
+        
+        user = auth.authenticate(username=username, password=password)
+        
+        if user is not None:
+            auth.login(request, user)
+            return redirect('/')
+        else:
+            messages.info(request, 'Credentials Invalid')
+            return redirect('login')
+    else:
+        return render(request, 'login.html')
+
+# I created a database connection to logout
+@login_required(login_url='login')
+def logout(request):
+    auth.logout(request)
+    return redirect('login')
